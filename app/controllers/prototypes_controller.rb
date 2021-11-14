2f77926b6659cd5219e519698d03b73c08e6e91b
tachaos <89284825+tachaos@users.noreply.github.com>
2021-11-14 16:22:32 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 
   def update
